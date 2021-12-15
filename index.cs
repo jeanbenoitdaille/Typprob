3d1040c7a312462fce13a66add37f035b0919ad1
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-15 15:07:52 +0100

    Add files via upload

---
+++ index.cs
@@ -0,0 +1,11 @@
+    using System;
+     
+    namespace Coding.Exercise
+    {
+        public class Exercise
+        {
+          public static float Divide(int a, int b){
+                return (float)a / (float)b;
+            }
+        }
+    }
